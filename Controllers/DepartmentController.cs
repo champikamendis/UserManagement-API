ec6eeea463b97f5d2cadde86b93d607efc105c65
Champika Mendis <champika@hatchyard.io>
2022-04-25 14:47:48 +0530

    Added department service

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi.Controllers;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApi.Helpers;
+using WebApi.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class DepartmentsController : ControllerBase
+{
+    private IDepartmentService _departmentService;
+    private IMapper _mapper;
+    private readonly AppSettings _appSettings;
+
+    public DepartmentsController(
+        IDepartmentService departmentService,
+        IMapper mapper,
+        IOptions<AppSettings> appSettings)
+    {
+        _departmentService = departmentService;
+        _mapper = mapper;
+        _appSettings = appSettings.Value;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var departments = _departmentService.GetAll();
+        return Ok(departments);
+    }
+}
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApi.Services;
+
+using AutoMapper;
+using WebApi.Entities;
+using WebApi.Helpers;
+
+public interface IDepartmentService
+{
+    IEnumerable<Department> GetAll();
+}
+
+public class DepartmentService : IDepartmentService
+{
+    private DataContext _context;
+    private readonly IMapper _mapper;
+
+    public DepartmentService(
+        DataContext context,
+        IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public IEnumerable<Department> GetAll()
+    {
+        return _context.Departments;
+    }
+}
